a11a9023c5425f026298f50bf0b5530b422a8012
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-03-28 21:37:18 +0200

    video source file added

---
+++ src/videoData.js
@@ -0,0 +1,5 @@
+export const videoSources = [
+    "./images/Dopalacze3_Uzaleznienie_popr2-cz1-1.mp4",
+    "./images/Dopalacze3_Uzaleznienie_popr2-cz2.mp4",
+    "./images/ekspert_film3-Ramsar_3000.mp4"
+]
